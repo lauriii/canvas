656f709530b7526af2c1da030888625864b8fddb
Wim Leers <44946-wimleers@users.noreply.drupalcode.org>
2024-08-29 16:31:45 +0000

    Issue #3469686 by Wim Leers, utkarsh_33, lauriii, bnjmnm: Changing default theme can break HTML forms generated by XB API routes: always negotiate the Stark theme

---
+++ src/Theme/XBThemeNegotiator.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\experience_builder\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+/**
+ * Determines the theme to be used for specific Experience Builder (XB) routes.
+ *
+ * This theme negotiator uses the `stark` theme for Experience Builder routes
+ * serving forms that are intended to be rendered using React, to guarantee
+ * predictable markup. Otherwise the Redux integration is likely to break.
+ *
+ * This also achieves an intentional side effect: nothing of Drupal themes
+ * is visible in the component props form or entity fields forms displayed in
+ * Experience Builder: `stark` defines no templates, and hence relies on all
+ * default templates only.
+ *
+ * @see themes/engines/semi_coupled/README.md
+ * @see ui/src/components/form/twig-to-jsx-component-map.js
+ * @see ui/src/components/form/inputBehaviors.tsx
+ */
+final class XBThemeNegotiator implements ThemeNegotiatorInterface {
+
+  /**
+   * The routes that this theme negotiator applies to.
+   *
+   * @var string[]
+   */
+  private static $routes = [
+    'experience_builder.component_props_form',
+    'experience_builder.api.entity_form',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $route_name = $route_match->getRouteName() ?? '';
+    return in_array($route_name, self::$routes, TRUE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    return 'stark';
+  }
+
+}
