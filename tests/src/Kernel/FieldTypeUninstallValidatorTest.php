6fd89f0ddd72cd108788cf2c21b3539ca931bad2
AKHIL BABU <61542-AkhilBabu@users.noreply.drupalcode.org>
2024-09-20 02:18:10 +0000

    Issue #3471028 by deepakkm, akhil babu, tedbow: Expand test coverage in FieldUninstallValidatorTest

--- tests/src/Kernel/FieldTypeUninstallValidatorTest.php
+++ tests/src/Kernel/FieldTypeUninstallValidatorTest.php
@@ -7,6 +7,7 @@ namespace Drupal\Tests\experience_builder\Kernel;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Extension\ModuleUninstallValidatorException;
 use Drupal\experience_builder\Plugin\DataType\ComponentTreeStructure;
+use Drupal\field\Entity\FieldConfig;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
 use Drupal\Tests\experience_builder\Traits\ContribStrictConfigSchemaTestTrait;
@@ -18,7 +19,6 @@ use Drupal\Tests\node\Traits\NodeCreationTrait;
  * @group experience_builder
  *
  * @todo Add extra test cases
- *   - Field is used in previous revision.
  *   - Default value stores the expression unescaped(is that possible?).
  *   - Field used in multiple entities of different types.
  *   - Others?
@@ -51,7 +51,7 @@ final class FieldTypeUninstallValidatorTest extends KernelTestBase {
    * Tests the FieldUninstallValidator.
    */
   public function testUninstall(): void {
-    $this->container->get('module_installer')->install(['experience_builder', 'link', 'node', 'text', 'xb_test_config_node_article', 'image', 'sdc_test']);
+    $this->container->get('module_installer')->install(['experience_builder', 'xb_test_config_node_article', 'sdc_test']);
     $node = $this->createNode([
       'title' => 'Test node',
       'type' => 'article',
@@ -83,10 +83,70 @@ final class FieldTypeUninstallValidatorTest extends KernelTestBase {
         ]),
       ],
     ]);
+
     $this->assertInstanceOf(Node::class, $node);
-    $this->expectException(ModuleUninstallValidatorException::class);
-    // For now match crude messages that prove we have caught both default and content uses.
-    $this->expectExceptionMessage('Provides a field type, <em class="placeholder">link</em>, that is in use in the content of the following entities: <em class="placeholder">node</em> id=<em class="placeholder">1</em> revision=<em class="placeholder">1</em>, Provides a field type, <em class="placeholder">link</em>, that is in use in the content of the following entities: <em class="placeholder">node</em> id=<em class="placeholder">1</em> revision=<em class="placeholder">1</em>, Provides a field type, <em class="placeholder">link</em>, that is in use in the default value of the following fields: <em class="placeholder">field_xb_test</em>');
+    try {
+      $this->container->get('module_installer')->uninstall(['link']);
+      $this->fail('Expected an exception');
+    }
+    catch (ModuleUninstallValidatorException $exception) {
+      // Assert exception message mentions but the content entity and the default value.
+      $this->assertStringContainsString('The following reasons prevent the modules from being uninstalled: Provides a field type, <em class="placeholder">link</em>, that is in use in the content of the following entities: <em class="placeholder">node</em> id=<em class="placeholder">1</em> revision=<em class="placeholder">1</em>, Provides a field type, <em class="placeholder">link</em>, that is in use in the content of the following entities: <em class="placeholder">node</em> id=<em class="placeholder">1</em> revision=<em class="placeholder">1</em>, Provides a field type, <em class="placeholder">link</em>, that is in use in the default value of the following fields: <em class="placeholder">field_xb_test</em>', $exception->getMessage());
+    }
+
+    $component_tree_without_link = [
+      'tree' => self::encodeXBData([
+        ComponentTreeStructure::ROOT_UUID => [
+          [
+            'uuid' => 'dynamic-static-card2df',
+            'component' => 'sdc_test:my-cta',
+          ],
+        ],
+      ]),
+      'props' => self::encodeXBData([
+        'dynamic-static-card2df' => [
+          'text' => [
+            'sourceType' => 'dynamic',
+            'expression' => 'ℹ︎␜entity:node:article␝title␞␟value',
+          ],
+        ],
+      ]),
+    ];
+
+    // Save a new revision that does not use the 'link' field.
+    $node->set('field_xb_test', $component_tree_without_link)->setNewRevision();
+    $node->save();
+
+    try {
+      $this->container->get('module_installer')->uninstall(['link']);
+      $this->fail('Expected an exception');
+    }
+    catch (ModuleUninstallValidatorException $exception) {
+      // Assert exception message mentions but the content entity and the default value.
+      $this->assertStringContainsString('The following reasons prevent the modules from being uninstalled: Provides a field type, <em class="placeholder">link</em>, that is in use in the content of the following entities: <em class="placeholder">node</em> id=<em class="placeholder">1</em> revision=<em class="placeholder">1</em>, Provides a field type, <em class="placeholder">link</em>, that is in use in the default value of the following fields: <em class="placeholder">field_xb_test</em>', $exception->getMessage());
+    }
+
+    // Delete the previous revision that used the 'link' field.
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+    $storage->deleteRevision(1);
+
+    try {
+      $this->container->get('module_installer')->uninstall(['link']);
+      $this->fail('Expected an exception');
+    }
+    catch (ModuleUninstallValidatorException $exception) {
+      // Assert exception message no longer mentions the content entity.
+      $this->assertStringContainsString('The following reasons prevent the modules from being uninstalled: Provides a field type, <em class="placeholder">link</em>, that is in use in the default value of the following fields: <em class="placeholder">field_xb_test</em>', $exception->getMessage());
+    }
+
+    // Update default value for component to not use the 'link' field.
+    $field_config = FieldConfig::loadByName('node', 'article', 'field_xb_test');
+    $this->assertInstanceOf(FieldConfig::class, $field_config);
+    $field_config->setDefaultValue($component_tree_without_link);
+    $field_config->save();
+
+    // Now since the neither the revision nor the default value contain the link
+    // field, the 'link' module can be uninstalled without an error.
     $this->container->get('module_installer')->uninstall(['link']);
   }
 
