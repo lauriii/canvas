bcf659bada4f54811119f5f97d82bcd00afd1a1c
Travis Carden <travis.carden@gmail.com>
2024-10-04 13:44:41 -0400

    Issue #3478830 by traviscarden: Remove dead `ApiResponseValidator::performXbValidation()`

--- src/EventSubscriber/ApiResponseValidator.php
+++ src/EventSubscriber/ApiResponseValidator.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Drupal\experience_builder\EventSubscriber;
 
 use League\OpenAPIValidation\PSR7\OperationAddress;
-use League\OpenAPIValidation\PSR7\ResponseValidator;
-use League\OpenAPIValidation\PSR7\SpecFinder;
 use League\OpenAPIValidation\PSR7\ValidatorBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -52,28 +49,7 @@ final class ApiResponseValidator extends ApiMessageValidatorBase {
     $psr7_response = $this->httpMessageFactory
       ->createResponse($response);
 
-    $this->performXbValidation($validator, $operation, $response);
     $validator->validate($operation, $psr7_response);
-  }
-
-  private function performXbValidation(ResponseValidator $validator, OperationAddress $operation, Response $response): void {
-    $schema = $validator->getSchema();
-    $spec_finder = new SpecFinder($schema);
-    $path = $spec_finder->findPathSpec($operation);
-
-    if ($operation->method() === 'get' && isset($path->get) && isset($path->get->responses[$response->getStatusCode()])) {
-      $extensions = $path->get->responses[$response->getStatusCode()]->getExtensions();
-      if (isset($extensions['x-xb-validation'])) {
-        assert(isset($extensions['x-xb-validation']['method']), 'Method not found in x-xb-validation extension.');
-        assert(method_exists(static::class, $extensions['x-xb-validation']['method']));
-        $content = $response->getContent();
-        assert(is_string($content));
-        $args = array_merge([json_decode($content, TRUE)], $extensions['x-xb-validation']['arguments'] ?? []);
-        $callback = [$this, $extensions['x-xb-validation']['method']];
-        assert(is_callable($callback));
-        call_user_func_array($callback, $args);
-      }
-    }
   }
 
 }
