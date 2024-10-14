2f84d78eb6975add816088e35c4dfe0aeff7520e
Atul Dubey <39000-soaratul@users.noreply.drupalcode.org>
2024-10-14 10:48:25 +0000

    Issue #3479895 by soaratul: Client-side warning due to insufficient use of @radix-ui/react-slot

--- ui/src/components/Panel.tsx
+++ ui/src/components/Panel.tsx
@@ -1,14 +1,19 @@
-import type React from 'react';
+import React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { Box } from '@radix-ui/themes';
 import clsx from 'clsx';
-
 import styles from './Panel.module.css';
 
-const Panel: React.FC<React.ComponentProps<typeof Box>> = (props) => {
+const Panel = React.forwardRef<
+  React.ElementRef<typeof Box>,
+  React.ComponentProps<typeof Box>
+>((props, ref) => {
   const { asChild, className } = props;
   const Comp = asChild ? Slot : Box;
-  return <Comp {...props} className={clsx(styles.panel, className)} />;
-};
+
+  return (
+    <Comp {...props} ref={ref} className={clsx(styles.panel, className)} />
+  );
+});
 
 export default Panel;
