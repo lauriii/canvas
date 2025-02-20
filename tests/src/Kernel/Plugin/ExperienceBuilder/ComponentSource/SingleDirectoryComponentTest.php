a10dae808a9ee9b091a5542a8a807bdb157bfb7c
Dave Long <24510-longwave@users.noreply.drupalcode.org>
2025-02-20 15:27:02 +0000

    Issue #3508003 by longwave, wim leers: Harden SingleDirectoryComponentTest against unexpected module paths

--- tests/src/Kernel/Plugin/ExperienceBuilder/ComponentSource/SingleDirectoryComponentTest.php
+++ tests/src/Kernel/Plugin/ExperienceBuilder/ComponentSource/SingleDirectoryComponentTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\experience_builder\Kernel\Plugin\ExperienceBuilder\ComponentSource;
 
+use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\Core\Theme\ComponentPluginManager;
 use Drupal\experience_builder\Plugin\ExperienceBuilder\ComponentSource\SingleDirectoryComponent;
 use Drupal\KernelTests\KernelTestBase;
@@ -35,14 +36,16 @@ final class SingleDirectoryComponentTest extends KernelTestBase {
     self::assertInstanceOf(SingleDirectoryComponent::class, $source);
 
     // Assert that existing files are rewritten to include the module path.
-    self::assertStringEndsWith('/experience_builder/components/image/600x400.png', $source->rewriteExampleUrl('600x400.png'));
-    self::assertStringEndsWith('/experience_builder/components/image/600x400.png', $source->rewriteExampleUrl('/600x400.png'));
-    self::assertStringEndsWith('/experience_builder/tests/fixtures/600x400.png', $source->rewriteExampleUrl('../../tests/fixtures/600x400.png'));
+    $module_path = \Drupal::service(ModuleExtensionList::class)->getPath('experience_builder');
+    self::assertStringEndsWith($module_path . '/components/image/600x400.png', $source->rewriteExampleUrl('600x400.png'));
+    self::assertStringEndsWith($module_path . '/components/image/600x400.png', $source->rewriteExampleUrl('/600x400.png'));
+    self::assertStringEndsWith($module_path . '/tests/fixtures/600x400.png', $source->rewriteExampleUrl('../../tests/fixtures/600x400.png'));
 
     // Assert that non-existing links have a leading slash but do not include the module nor SDC path.
     $url = $source->rewriteExampleUrl('test/path');
     self::assertStringEndsWith('/test/path', $url);
-    self::assertStringNotContainsString('experience_builder/components', $url);
+    self::assertStringNotContainsString($module_path, $url);
+    self::assertStringNotContainsString('components', $url);
 
     // Assert that non-existing links with a leading slash are not doubled.
     $url = $source->rewriteExampleUrl('/test/path');
