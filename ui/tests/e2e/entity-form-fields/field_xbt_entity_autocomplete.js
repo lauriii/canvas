ec52016e88091324cbc994e73cab676645522778
Lee Rowlands <23667-larowlan@users.noreply.drupalcode.org>
2025-04-12 01:09:15 +0000

    Issue #3517858 by larowlan: Add e2e tests for entity reference autocomplete in page data form

---
+++ ui/tests/e2e/entity-form-fields/field_xbt_entity_autocomplete.js
@@ -0,0 +1,25 @@
+export const edit = (cy) => {
+  cy.findByLabelText('XB Entity Reference (Autocomplete)').as('autocomplete');
+  cy.get('@autocomplete').should(
+    'have.value',
+    'XB With a block in the layout (3)',
+  );
+  cy.get('@autocomplete').clear();
+  cy.get('@autocomplete').type('XB Needs This For');
+  cy.get('ul.ui-autocomplete').should('exist');
+  cy.get('ul.ui-autocomplete li').should(
+    'have.text',
+    'XB Needs This For The Time Being',
+  );
+  cy.get('ul.ui-autocomplete li').click();
+  cy.get('@autocomplete').should(
+    'have.value',
+    'XB Needs This For The Time Being (1)',
+  );
+};
+export const assertData = (response) => {
+  expect(
+    response.relationships.field_xbt_entity_ref_auto.data.meta
+      .drupal_internal__target_id,
+  ).to.equal(1);
+};
