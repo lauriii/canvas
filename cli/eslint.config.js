42cfea1d5532cf74c46958d7c2d75fe2ec31efc2
Harumi Jang <57237-hooroomoo@users.noreply.drupalcode.org>
2025-06-09 21:57:49 +0000

    Issue #3525574 by hooroomoo, balintbrews: Create initial codebase for CLI tool

---
+++ cli/eslint.config.js
@@ -0,0 +1,50 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['**/dist', '**/node_modules'],
+  },
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      parserOptions: {
+        project: true,
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        2,
+        {
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+
+      'no-shadow': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { args: 'none', caughtErrors: 'none' },
+      ],
+      'no-redeclare': ['error', { builtinGlobals: false }],
+    },
+  },
+];
