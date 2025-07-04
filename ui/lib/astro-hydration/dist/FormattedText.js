272794d73a28320fb9989850dcb3d60150b0ce41
Balint Kleri <b@balintbrews.com>
2025-07-04 21:24:50 +0200

    Back to dev.

--- ui/lib/astro-hydration/dist/FormattedText.js
+++
@@ -1,1 +0,0 @@
-import{jsx as e}from"./jsxRuntime.module.js";import"./preact.module.js";function p({children:t,as:o="div",...n}){return e(o,{dangerouslySetInnerHTML:{__html:t},...n})}export{p as default};
