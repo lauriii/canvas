ac1bb07498b0013c684062651e2b976659c9d98e
Balint Kleri <b@balintbrews.com>
2025-07-04 21:24:50 +0200

    Experience Builder 0.5.0-alpha1

---
+++ ui/lib/astro-hydration/dist/FormattedText.js
@@ -0,0 +1,1 @@
+import{jsx as e}from"./jsxRuntime.module.js";import"./preact.module.js";function p({children:t,as:o="div",...n}){return e(o,{dangerouslySetInnerHTML:{__html:t},...n})}export{p as default};
