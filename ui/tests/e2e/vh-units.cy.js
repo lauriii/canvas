c0330f104f6808f570c485cee27b7c5cd5530018
Jesse Baker <67266-jessebaker@users.noreply.drupalcode.org>
2025-08-12 08:40:33 +0000

    Issue #3535072 by jessebaker, bnjmnm, mglaman: Drupal.behaviors.xbDisableLinks doesn't disable links added to the DOM later

--- ui/tests/e2e/vh-units.cy.js
+++ ui/tests/e2e/vh-units.cy.js
@@ -1,4 +1,4 @@
-describe('Preview a page', () => {
+describe('Vh units should not cause issues', () => {
   before(() => {
     cy.drupalXbInstall(['xb_test_vh_preview']);
   });
@@ -11,7 +11,7 @@ describe('Preview a page', () => {
     cy.drupalUninstall();
   });
 
-  it('handles elements that have height defined in vh units', () => {
+  it('does not continually increase the height of the iframe when there are elements that have height defined in vh units', () => {
     cy.loadURLandWaitForXBLoaded({ url: 'xb/node/2' });
     cy.openLibraryPanel();
     cy.get('.primaryPanelContent').findByText('Hero').click();
@@ -49,66 +49,5 @@ describe('Preview a page', () => {
     cy.testInIframe('#vh-full', (vhDiv) => {
       expect(vhDiv.getBoundingClientRect().height).to.be.closeTo(768, 10);
     });
-  });
-
-  it('Can view a preview', () => {
-    cy.loadURLandWaitForXBLoaded();
-
-    cy.findByText('Preview').click();
-
-    cy.findByText('Exit Preview');
-
-    cy.get('iframe[title="Page preview"]')
-      .its('0.contentDocument.body')
-      .should('not.be.empty')
-      .then(cy.wrap)
-      .within(() => {
-        cy.get('.my-hero__heading').should('exist');
-      });
-
-    cy.findAllByText('Tablet').filter(':visible').click();
-
-    cy.url().should('contain', `/xb/node/1/preview/tablet`);
-
-    cy.get('iframe[title="Page preview"]').should(
-      'have.css',
-      'width',
-      '1024px',
-    );
-
-    cy.findByText('Exit Preview').click();
-    cy.previewReady();
-  });
-
-  it('Links are intercepted and a modal is shown', () => {
-    cy.loadURLandWaitForXBLoaded();
-
-    cy.clickComponentInPreview('Hero', 0);
-
-    cy.findByLabelText('CTA 1 text').clear();
-    cy.findByLabelText('CTA 1 text').type('Link to Drupal');
-    cy.findByLabelText('CTA 1 text').blur();
-
-    cy.waitForElementContentInIframe('a.my-hero__cta', 'Link to Drupal');
-
-    cy.findByText('Preview').click();
-
-    cy.findByText('Exit Preview');
-
-    cy.get('iframe[title="Page preview"]')
-      .its('0.contentDocument.body')
-      .should('not.be.empty')
-      .then(cy.wrap)
-      .within(() => {
-        cy.findByText('Link to Drupal').should(
-          'have.attr',
-          'data-once',
-          'xbDisableLinks',
-        );
-        cy.findByText('Link to Drupal').click();
-      });
-
-    cy.findByText('https://drupal.org/');
-    cy.findByText('Open in new window');
   });
 });
