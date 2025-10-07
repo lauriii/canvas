0a6f6d05125fcdee23300c02dfc39b7243076885
Jesse Baker <67266-jessebaker@users.noreply.drupalcode.org>
2025-10-07 16:30:46 +0000

    [#3550552] feat: AI image upload Playwright test often, but not always fails (flakey) - Removed 1 common case of test flake

--- tests/src/Playwright/objects/Ai.ts
+++ tests/src/Playwright/objects/Ai.ts
@@ -9,6 +9,17 @@ export class Ai {
 
   async openPanel() {
     await this.page.getByRole('button', { name: 'Open AI Panel' }).click();
+    await this.page.locator('deep-chat').evaluate((el) => {
+      const shadowRoot = el.shadowRoot;
+      if (!shadowRoot) {
+        throw new Error('No shadow root found');
+      }
+      const textInputDiv = shadowRoot.querySelector('div#text-input');
+
+      if (!textInputDiv) {
+        throw new Error('No div with id "text-input" found in shadow root');
+      }
+    });
   }
 
   async submitQuery(query: string) {
