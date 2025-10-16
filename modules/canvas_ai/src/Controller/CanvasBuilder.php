e90e348bfe94e21bd0fe798f63831188eac65903
Tim Plunkett <41843-tim.plunkett@users.noreply.drupalcode.org>
2025-10-16 10:57:58 +0000

    [#3552355] refactor(AI): Make call to $agent->setTokenContexts() more maintainable
    By: tim.plunkett

--- modules/canvas_ai/src/Controller/CanvasBuilder.php
+++ modules/canvas_ai/src/Controller/CanvasBuilder.php
@@ -220,9 +220,18 @@ final class CanvasBuilder extends ControllerBase {
     $agent->setModelName($default['model_id']);
     $agent->setAiConfiguration([]);
     $agent->setCreateDirectly(TRUE);
-    $menu_fetch_source = $this->getMenuFetchSource();
-    $json_api_module_status = $this->moduleHandler()->moduleExists('jsonapi') ? 'enabled' : 'disabled';
-    $agent->setTokenContexts(['entity_type' => $prompt['entity_type'] ?? NULL, 'entity_id' => $prompt['entity_id'] ?? NULL, 'selected_component' => $prompt['selected_component'] ?? NULL, 'layout' => $prompt['layout'] ?? NULL, 'derived_proptypes' => isset($prompt['derived_proptypes']) ? JSON::encode($prompt['derived_proptypes']) : NULL, 'page_title' => $prompt['page_title'] ?? NULL, 'page_description' => $prompt['page_description'] ?? NULL, 'active_component_uuid' => $prompt['active_component_uuid'] ?? 'None', 'menu_fetch_source' => $menu_fetch_source, 'json_api_module_status' => $json_api_module_status]);
+    $agent->setTokenContexts([
+      'entity_type' => $prompt['entity_type'] ?? NULL,
+      'entity_id' => $prompt['entity_id'] ?? NULL,
+      'selected_component' => $prompt['selected_component'] ?? NULL,
+      'layout' => $prompt['layout'] ?? NULL,
+      'derived_proptypes' => isset($prompt['derived_proptypes']) ? Json::encode($prompt['derived_proptypes']) : NULL,
+      'page_title' => $prompt['page_title'] ?? NULL,
+      'page_description' => $prompt['page_description'] ?? NULL,
+      'active_component_uuid' => $prompt['active_component_uuid'] ?? 'None',
+      'menu_fetch_source' => $this->getMenuFetchSource(),
+      'json_api_module_status' => $this->moduleHandler()->moduleExists('jsonapi') ? 'enabled' : 'disabled',
+    ]);
     try {
       $solvability = $agent->determineSolvability();
     }
